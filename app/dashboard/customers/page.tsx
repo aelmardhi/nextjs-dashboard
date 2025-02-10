c1562192e05f6ed20c624b63aa19be1e12f31f50
aelmardhi <aelmardhi@gmail.com>
2025-02-10 14:28:07 +0200

    add: customers skeleton

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,39 @@
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import CustomersTable from '@/app/ui/customers/table';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons'
 import { Metadata } from 'next';
- 
+import { Suspense } from 'react';
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page(){
-    return <p>Customers Page</p>
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string,
+    pagr?: string,
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <div className="mt-6 flow-root">
+        <div className="overflow-x-auto">
+          <div className="inline-block min-w-full align-middle">
+            <div className="overflow-hidden rounded-md bg-gray-50 p-2 md:pt-0">
+              <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+                <CustomersTable query={query}></CustomersTable>
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>);
 }
